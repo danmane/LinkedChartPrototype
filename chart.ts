52b879f87cc027b1a7adf2217afd19f80c4fc278
Daniel Mane <danmane@gmail.com>
2013-12-13 15:25:52 -0800

    updated chart, now lays out grid, range axis oriented correctly

--- chart.ts
+++ chart.ts
@@ -6,15 +6,15 @@ interface ITimeseriesDatum {
 }
 
 interface IWeatherDatum {
-    avg : number; // Average temperature on date
+    avg: number; // Average temperature on date
     avgh: number;
     avgl: number;
-    hi  : number;
-    hih : number;
-    hil : number;
-    lo  : number;
-    loh : number;
-    lol : number;
+    hi: number;
+    hih: number;
+    hil: number;
+    lo: number;
+    loh: number;
+    lol: number;
     precip: number;
     day: number;
     date: Date;
@@ -25,7 +25,7 @@ class Chart {
     public static margin = { top: 20, right: 20, bottom: 30, left: 30 };
     public static width = 300 - Chart.margin.left - Chart.margin.right;
     public static height = 300 - Chart.margin.top - Chart.margin.bottom;
-    private static attributes = ["avg","avgh","avgl","hi","hih","hil","lo","loh","lol","precip","day"]
+    private static attributes = ["avg", "avgh", "avgl", "hi", "hih", "hil", "lo", "loh", "lol", "precip", "day"]
     private static parseDate = d3.time.format("%Y-%m-%d").parse;
 
     public svg: D3.Selection;
@@ -55,14 +55,13 @@ class Chart {
         return <IWeatherDatum[]> indata;
     }
 
-    constructor(container: D3.Selection, url: string) {
+    constructor(container: D3.Selection, url: string, public height: number, public width: number) {
         this.setupD3Objects();
         this.setupDOM(container);
         d3.csv(url, (error, data) => {
             this.data = Chart.processCSVData(data);
             this.initialRender();
-          });
-
+        });
     }
 
     private setupD3Objects() {
@@ -87,14 +86,18 @@ class Chart {
     }
 
     private setupDOM(container: D3.Selection) {
-        this.svg = container.append("svg");
+        this.height -= Chart.margin.top  + Chart.margin.bottom;
+        this.width  -= Chart.margin.left + Chart.margin.right;
+        this.svg = container.append("svg")
+            .attr("height", this.height)
+            .attr("width",  this.width);
 
         this.xAxisEl = this.svg.append("g")
-            .attr("class", "x axis")
+            .attr("class", "x-axis")
             .attr("transform", "translate(0," + Chart.height + ")");
 
         this.yAxisEl = this.svg.append("g")
-            .attr("class", "y axis");
+            .attr("class", "y-axis");
 
         this.plot = this.svg.append("g");
 
@@ -105,7 +108,7 @@ class Chart {
 
     private initialRender() {
         var dateDomain = d3.extent(this.data, function(d) { return d.date; });
-        var rangeDomain = [-10, 100];
+        var rangeDomain = [100, 0];
         this.xScalePlot.domain(dateDomain);
         this.xScaleAxis.domain(dateDomain);
         this.yScalePlot.domain(rangeDomain);
@@ -123,29 +126,29 @@ class Chart {
             .attr("class", "line")
             .attr("d", this.lines[2]);
 
+        // var width =
+
     }
 }
 
 class ChartGen {
-    public containerSelection: D3.Selection;
     public charts: Chart[];
-
     constructor(public numCharts: number) {
-      this.containerSelection = d3.select("body");
-      this.charts = [];
-      var i=0;
-      d3.json("Data/cityNames.json", (error, data) => {
-
-          this.makeCharts(this.numCharts, d3.values(data));
-
+        this.charts = [];
+        d3.json("Data/cityNames.json", (error, data) => {
+            this.makeCharts(this.numCharts, d3.values(data));
         });
     }
 
-    public makeCharts(numCharts: number, fileNames: string[]){
-      fileNames = fileNames.slice(0, numCharts);
-      fileNames.forEach((fileName: string) => {
-          fileName = "Data/" + fileName;
-          this.charts.push(new Chart(this.containerSelection, fileName));
+    public makeCharts(numCharts: number, fileNames: string[]) {
+        var containerSelection = d3.select("body");
+        var chartsToSide = Math.ceil(Math.sqrt(this.numCharts));
+        var width  = window.innerWidth  / chartsToSide;
+        var height = window.innerHeight / chartsToSide;
+        fileNames = fileNames.slice(0, numCharts);
+        fileNames.forEach((fileName: string) => {
+            fileName = "Data/" + fileName;
+            this.charts.push(new Chart(containerSelection, fileName, height, width));
         });
     }
 }
