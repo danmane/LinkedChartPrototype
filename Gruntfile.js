c7f0a8f03cf8a64dad344a4f14de806416e2e409
Daniel Mane <danmane@gmail.com>
2013-12-13 16:02:00 -0800

    Setup grunt, grunt watch :)

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+/*
+ * The Web project Gruntfile.
+ *
+ * Copyright 2013 Palantir Technologies, Inc. All rights reserved.
+ */
+
+module.exports = function(grunt) {
+  "use strict";
+
+  var path = require("path");
+  var cwd = process.cwd();
+
+  // project configuration
+  grunt.initConfig({
+
+    typescript: {
+      compile: {
+        options: {
+          module: "amd",
+          // nolib: true,
+          sourcemap: false,
+          target: "ES5"
+        },
+        src: ["chart.ts"],
+        dest: "."
+      }
+    },
+    watch: {
+      "rebuild": {
+        "tasks": [
+          "build:rebuild"
+        ],
+        "files": [
+          "Gruntfile.js",
+          "chart.ts"
+        ]
+      }
+    }
+  });
+
+
+  require('load-grunt-tasks')(grunt);
+
+
+
+  // default task (this is what runs when a task isn't specified)
+  grunt.registerTask("default", "build");
+
+  grunt.registerTask("build",
+    [
+      "typescript"
+    ]
+  );
+};
