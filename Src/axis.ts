cdfe2c98e86b00d4aa9cdffae620400ca5949e53
Daniel Mane <danmane@gmail.com>
2013-12-20 14:48:38 -0500

    Spacing changes

--- Src/axis.ts
+++ Src/axis.ts
@@ -1,25 +1,25 @@
 ///<reference path="../Lib/d3.d.ts" />
 
 module Axis {
-	export class Axis {
-		public d3axis: D3.Svg.Axis;
-		public axisEl: D3.Selection;
-		
-		constructor(
-			public container: D3.Selection,
-			public scale: D3.Scale.Scale, 
-			public orientation: string,
-			public formatter: any
-		) {
-			this.d3axis = d3.svg.axis().scale(this.scale).orient(this.orientation);
-			this.axisEl = this.container.append("g").classed("axis", true);
-			if (this.formatter != null) {
-				this.d3axis.tickFormat(formatter);
-			}
-		}
+  export class Axis {
+    public d3axis: D3.Svg.Axis;
+    public axisEl: D3.Selection;
 
-		public render() {
-			this.axisEl.call(this.d3axis);
-		}
-	}
+    constructor(
+      public container: D3.Selection,
+      public scale: D3.Scale.Scale,
+      public orientation: string,
+      public formatter: any
+    ) {
+      this.d3axis = d3.svg.axis().scale(this.scale).orient(this.orientation);
+      this.axisEl = this.container.append("g").classed("axis", true);
+      if (this.formatter != null) {
+        this.d3axis.tickFormat(formatter);
+      }
+    }
+
+    public render() {
+      this.axisEl.call(this.d3axis);
+    }
+  }
 }
