6c06db6820bacaa065e99a4f8724e6cb8c550b39
Daniel Mané <danmane@gmail.com>
2013-12-23 23:18:57 -0500

    Fix spacing on axis

--- Src/axis.ts
+++ Src/axis.ts
@@ -30,72 +30,7 @@ module Axis {
     }
 
     public render() {
-      var g = this.axisEl;
-      var ticks = this.scale.ticks(10);
-      var tickFormat = this.scale.tickFormat(10);
-      var tick = g.selectAll(".tick").data(ticks, this.scale);
-      var tickEnter = tick.enter().insert("g", ".domain")
-        .attr("class", "tick").style("opacity", 1e-6);
-      var tickExit = d3.transition(tick.exit()).style("opacity", 1e-6).remove();
-      var tickUpdate = d3.transition(tick).style("opacity", 1);
-      var range = this.scale.range();
-      var path = g.selectAll(".domain").data([0]);
-      var pathUpdate = (path.enter().append("path").attr("class", "domain"), d3.transition(path));
-      
-      tickEnter.append("line");
-      tickEnter.append("text");
-
-      var lineEnter  = tickEnter .select("line");
-      var lineUpdate = tickUpdate.select("line");
-      var text = tick.select("text").text(tickFormat);
-      var textEnter  = tickEnter .select("text");
-      var textUpdate = tickUpdate.select("text");
-      var tickTransform;
-      var orient = this.orientation;
-      switch (orient) {
-        case "bottom": {
-          tickTransform = d3_svg_axisX;
-          lineEnter.attr("y2", innerTickSize);
-          textEnter.attr("y", Math.max(innerTickSize, 0) + tickPadding);
-          lineUpdate.attr("x2", 0).attr("y2", innerTickSize);
-          textUpdate.attr("x", 0).attr("y", Math.max(innerTickSize, 0) + tickPadding);
-          text.attr("dy", ".71em").style("text-anchor", "middle");
-          pathUpdate.attr("d", "M" + range[0] + "," + outerTickSize + "V0H" + range[1] + "V" + outerTickSize);
-          break;
-        }
-        case "top": {
-          tickTransform = d3_svg_axisX;
-          lineEnter.attr("y2", -innerTickSize);
-          textEnter.attr("y", -(Math.max(innerTickSize, 0) + tickPadding));
-          lineUpdate.attr("x2", 0).attr("y2", -innerTickSize);
-          textUpdate.attr("x", 0).attr("y", -(Math.max(innerTickSize, 0) + tickPadding));
-          text.attr("dy", "0em").style("text-anchor", "middle");
-          pathUpdate.attr("d", "M" + range[0] + "," + -outerTickSize + "V0H" + range[1] + "V" + -outerTickSize);
-          break;
-        }
-        case "left": {
-          tickTransform = d3_svg_axisY;
-          lineEnter.attr("x2", -innerTickSize);
-          textEnter.attr("x", -(Math.max(innerTickSize, 0) + tickPadding));
-          lineUpdate.attr("x2", -innerTickSize).attr("y2", 0);
-          textUpdate.attr("x", -(Math.max(innerTickSize, 0) + tickPadding)).attr("y", 0);
-          text.attr("dy", ".32em").style("text-anchor", "end");
-          pathUpdate.attr("d", "M" + -outerTickSize + "," + range[0] + "H0V" + range[1] + "H" + -outerTickSize);
-          break;
-        }
-        case "right": {
-          tickTransform = d3_svg_axisY;
-          lineEnter.attr("x2", innerTickSize);
-          textEnter.attr("x", Math.max(innerTickSize, 0) + tickPadding);
-          lineUpdate.attr("x2", innerTickSize).attr("y2", 0);
-          textUpdate.attr("x", Math.max(innerTickSize, 0) + tickPadding).attr("y", 0);
-          text.attr("dy", ".32em").style("text-anchor", "start");
-          pathUpdate.attr("d", "M" + outerTickSize + "," + range[0] + "H0V" + range[1] + "H" + outerTickSize);
-          break;
-        }
-      }
-
-
+      this.axisEl.call(this.d3axis);
       this.axisEl.attr("transform","");
     }
 
